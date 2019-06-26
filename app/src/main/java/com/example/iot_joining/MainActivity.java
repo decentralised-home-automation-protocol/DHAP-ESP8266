589af1e2fc4ee8db5006599a3c0f6c862e1e61da
Daniel Milner <danielmmilner@gmail.com>
2019-06-26 13:33:03 +1000

    App can now scan for wifi networks

--- app/src/main/java/com/example/iot_joining/MainActivity.java
+++ app/src/main/java/com/example/iot_joining/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.iot_joining;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "results";
+
+    WifiManager wifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+
+        wifiManager.startScan();
+        List<ScanResult> results = wifiManager.getScanResults();
+        for (ScanResult r : results) {
+            Log.e(TAG, "scanSuccess: " + r.SSID);
+        }
     }
 }
