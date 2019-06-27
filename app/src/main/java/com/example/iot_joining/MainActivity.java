d1a6c09e22c1994c687c44108e21aacb0b68adce
Daniel Milner <danielmmilner@gmail.com>
2019-06-27 17:13:21 +1000

    Added a password dialog box

--- app/src/main/java/com/example/iot_joining/MainActivity.java
+++ app/src/main/java/com/example/iot_joining/MainActivity.java
@@ -1,29 +1,32 @@
 package com.example.iot_joining;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = "results";
-
-    LinearLayout foundNetworks;
-
-    WifiManager wifiManager;
+    private LinearLayout foundNetworks;
+    private WifiManager wifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         foundNetworks = findViewById(R.id.found_networks);
 
@@ -33,33 +36,52 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
     }
 
-    public void scanWiFI(View view) {
+    public void scanWiFI(final View view) {
         foundNetworks.removeAllViews();
         wifiManager.startScan();
         List<ScanResult> results = wifiManager.getScanResults();
-        for (final ScanResult r : results) {
+        for (final ScanResult result : results) {
 
             Button networkButton = new Button(getApplicationContext());
             networkButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     //Join wifi network.
-                    Log.e(TAG, "onClick: " + r.SSID);
-                    wifiManager.setWifiEnabled(true);
-
-                    WifiConfiguration wifiConfig = new WifiConfiguration();
-                    wifiConfig.SSID = String.format("\"%s\"", r.SSID);
-                    wifiConfig.preSharedKey = String.format("\"%s\"", "thereisnospoon");
-
-                    WifiManager wifiManager = (WifiManager)getSystemService(WIFI_SERVICE);
-                    int netId = wifiManager.addNetwork(wifiConfig);
-                    wifiManager.disconnect();
-                    wifiManager.enableNetwork(netId, true);
-                    wifiManager.reconnect();
+                    passwordDialog(result.SSID);
                 }
             });
 
-            networkButton.setText(r.SSID);
+            networkButton.setText(result.SSID);
             foundNetworks.addView(networkButton);
         }
+    }
+
+    private void passwordDialog(final String SSID) {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View formElementsView = inflater.inflate(R.layout.password_dialog, null, false);
+        final EditText passwordEditText = formElementsView.findViewById(R.id.passwordEditText);
+
+        new AlertDialog.Builder(MainActivity.this).setView(formElementsView)
+                .setTitle("Enter Password")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        joinNetwork(SSID, passwordEditText.getText().toString());
+                    }
+                }).show();
+    }
+
+    private void joinNetwork(String SSID, String password) {
+        Log.e(TAG, "joinNetwork: " + SSID + " password: " + password);
+        wifiManager.setWifiEnabled(true);
+
+        WifiConfiguration wifiConfig = new WifiConfiguration();
+        wifiConfig.SSID = String.format("\"%s\"", SSID);
+        wifiConfig.preSharedKey = String.format("\"%s\"", password);
+
+        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+        int netId = wifiManager.addNetwork(wifiConfig);
+        wifiManager.disconnect();
+        wifiManager.enableNetwork(netId, true);
+        wifiManager.reconnect();
     }
 }
