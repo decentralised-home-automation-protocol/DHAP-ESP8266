2f0ba384b1c03dc0ef12903bdea89e8d18df5108
Daniel Milner <danielmmilner@gmail.com>
2019-06-27 16:25:18 +1000

    Now enables wifi and connects to a device when button is pressed.

--- app/src/main/java/com/example/iot_joining/MainActivity.java
+++ app/src/main/java/com/example/iot_joining/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.iot_joining;
 import android.content.Context;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = "results";
+
+    LinearLayout foundNetworks;
 
     WifiManager wifiManager;
 
@@ -19,16 +25,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        foundNetworks = findViewById(R.id.found_networks);
 
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+    }
 
+    public void scanWiFI(View view) {
+        foundNetworks.removeAllViews();
         wifiManager.startScan();
         List<ScanResult> results = wifiManager.getScanResults();
-        for (ScanResult r : results) {
-            Log.e(TAG, "scanSuccess: " + r.SSID);
+        for (final ScanResult r : results) {
+
+            Button networkButton = new Button(getApplicationContext());
+            networkButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    //Join wifi network.
+                    Log.e(TAG, "onClick: " + r.SSID);
+                    wifiManager.setWifiEnabled(true);
+
+                    WifiConfiguration wifiConfig = new WifiConfiguration();
+                    wifiConfig.SSID = String.format("\"%s\"", r.SSID);
+                    wifiConfig.preSharedKey = String.format("\"%s\"", "thereisnospoon");
+
+                    WifiManager wifiManager = (WifiManager)getSystemService(WIFI_SERVICE);
+                    int netId = wifiManager.addNetwork(wifiConfig);
+                    wifiManager.disconnect();
+                    wifiManager.enableNetwork(netId, true);
+                    wifiManager.reconnect();
+                }
+            });
+
+            networkButton.setText(r.SSID);
+            foundNetworks.addView(networkButton);
         }
     }
 }
